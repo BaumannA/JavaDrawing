dc6001f855b0fb9720640489ee20f350cc8f5db5
BaumannA <anthony.baumann@icloud.com>
2016-12-02 21:15:59 -0600

    Stuff happened

--- Drawing/Main.java
+++ Drawing/Main.java
@@ -8,6 +8,8 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
+
 public class Main extends PApplet{
 
 	PShape rectangle;
@@ -16,22 +18,43 @@ public class Main extends PApplet{
 	
 	char letter;
 	
+	int paintSize = 50;
 	int color1;
 	int color2;
 	int color3;
 	boolean red = false;
+	boolean menuRed = false;
 	boolean blue = false;
 	boolean green = false;
 	boolean yellow = false;
 	boolean eraser = false;
 	boolean purple = false;
 	boolean brown = false;
+	boolean orange = false;
+	
+	boolean show_text = false;
+	boolean welcome = true;
+	boolean permText = false;
 	
 	boolean saving = false;
+	
+	int state = 0;
+	
+	public void menureset() {
+		if (menuRed == true) {
+			if (mousePressed) {
+				
+			}
+		}
+		else {
+			println("Uh oh spagetti O!");
+		}
+	}
 	
 	// needed to create this in order for Eclipse to work
 	public static void main(String[] args) {
 		PApplet.main("Main");
+		
 	}
 	
 	public void settings(){
@@ -39,39 +62,126 @@ public class Main extends PApplet{
 	}
 
 	public void setup() {
-		size(1280, 720);
-		smooth();
+	//	smooth();
 		background(255, 255, 255);
 		noStroke();
-		
+
+
 	}
 	
 	public void draw() {
-		while (mouseX >= 0 && mouseX <= 50 && mouseY >= 50 && mouseY <= 100) {
-			fill(255, 0, 0);
-			rect(50, 100, 50, 50);
-			break;
+		if(state == 0) {
+			text("Welcome to drawing", 500, 400, 150, 150);
 		}
-		if (mouseX >= 50) {
+		
+		if(state == 1) {
+			background(255);
+			state = 2;
+		}
+		
+		if (mousePressed && show_text == true) {
+			state = 1;
+			fill(color1, color2, color3);
+			text("Press 'c' to clear the screen", 50, 5, 200, 50);
+			text("Press 's' to save", 250, 5, 200, 50);
+			text("Press ']' to increase paint size and '[' to decrease paint size", 500, 5, 500, 50);
+			text("Current paint size: " + paintSize, 1000, 5, 150, 50);
+			text("easer", 10, 15, 200, 50 );
+			show_text = false;
+			permText = true;
+		}
+		
+		if(permText) {
+			fill(color1, color2, color3);
+			text("Press 'c' to clear the screen", 50, 5, 200, 50);
+			text("Press 's' to save", 250, 5, 200, 50);
+			text("Press ']' to increase paint size and '[' to decrease paint size", 500, 5, 500, 50);
+			text("Current paint size: " + paintSize, 1000, 5, 150, 50);
+			text("easer", 10, 15, 200, 50 );
+		}
+
+		if (mousePressed && state == 0) {
+			state = 1;
+			show_text = true;
+		}
+		
+		if (welcome == false) {
 			
+		}
+		
+		if (show_text == true) {
+			fill(color1, color2, color3);
+			text("Press 'c' to clear the screen", 50, 5, 200, 50);
+			text("Press 's' to save", 250, 5, 200, 50);
+			text("Press ']' to increase paint size and '[' to decrease paint size", 500, 5, 500, 50);
+			text("Current paint size: " + paintSize, 1000, 5, 150, 50);
+			text("easer", 10, 15, 200, 50 );
+			
+		}
+		
+		
+		if (mouseX >= 0 && mouseX <= 50 && mouseY >= 50 && mouseY <= 100) {
+			menuRed = true;
+
+			
+		}
+		
+		else {
+			fill(255);
+			menuRed = false;	
 		}
 
 		if (keyPressed) {
 			
+			if (paintSize >= 1 && paintSize <= 150) {
+				
+				if (key == ']') {
+					paintSize += 1;
+					fill(255);
+//					rect(1000, 5, 300, 20);
+				}
+				
+				if (key == '[') {
+					paintSize -= 1;
+					fill(255);
+//					rect(1000, 5, 300, 20);
+				}
+			}
+			
+			if (paintSize < 2) {
+				
+				if (key == ']') {
+					paintSize += 1;
+					fill(255);
+//					rect(1000, 5, 300, 20);
+				}
+			}
+			
+			if (paintSize > 150) {
+				
+				if (key == '[') {
+					paintSize -= 1;
+					fill(255);
+//					rect(1000, 5, 300, 20);
+				}
+			}
+			
 			if (key == 'c') {
+				permText = true;
+				
 				background(255, 255, 255);
 			}
 			
 			if (key == 's') {
 //				text("What do you want to call this file?", 400, 700, 200, 50);
-				save("Drawing.tif");
+				save("Drawing1.tif");
 				
 			}
 //			huge work in progress:
 //			if (saving == true) {
 //				text("What do you want to call this file?", 400, 700, 200, 50);
 //				text(file, 600, 700, 500, 50);
-//				if ((key >='A' && key <= 'z') || key == ' ') {
+//				while ((key >='A' && key <= 'z') || key == ' ') {
 //					letter = key;
 //					file = file + key;
 //					println(key);
@@ -86,10 +196,17 @@ public class Main extends PApplet{
 		else {
 			color1 = 0;
 		}
+		
+		if(menuRed) fill(255, 0, 0); rect(50, 50, 50, 50); 
+		
 		fill(0);
-		text("Press 'c' to clear the screen", 50, 700, 200, 50);
-		text("Press 's' to save", 250, 700, 200, 50);
-		text("easer", 10, 15, 200, 50 );
+//		rect(0, 700, 1280, 50);
+//		text("Press 'c' to clear the screen", 50, 5, 200, 50);
+//		text("Press 's' to save", 250, 5, 200, 50);
+//		text("Press ']' to increase paint size and '[' to decrease paint size", 500, 5, 500, 50);
+//		text("Current paint size: " + paintSize, 1000, 5, 150, 50);
+//		text("easer", 10, 15, 200, 50 );
+		smooth();
 		fill(255, 0, 0);
 		// red square
 		rect(0, 50, 50, 50);
@@ -108,6 +225,9 @@ public class Main extends PApplet{
 		fill(165, 42, 42);
 		// brown square
 		rect(0, 300, 50, 50);
+		fill(255, 128, 0);
+		// oragne square
+		rect(0, 350, 50, 50);
 		fill(0);
 		
 		
@@ -149,14 +269,19 @@ public class Main extends PApplet{
 			color2 = 42;
 			color3 = 42;
 		}
+		if(orange) {
+			color1 = 255;
+			color2 = 128;
+			color3 = 0;
+		}
 		else{
 			fill(0);
 		}
 		// check if mouse is in drawing area
-		if (mouseX >= 50 && mouseX <= 1280 && mouseY >= 0 && mouseY <= 680) {
+		if (mouseX >= 50 && mouseX <= 1280 && mouseY >= 50 && mouseY <= 680) {
 			// change the drawing color 
 			fill(color1, color2, color3);
-			rect(mouseX, mouseY, 50, 50);
+			rect(mouseX, mouseY, paintSize, paintSize);
 		}
 		// if red
 		if (mouseX >= 0 && mouseX <= 50 && mouseY >= 50 && mouseY <= 100) {
@@ -166,6 +291,8 @@ public class Main extends PApplet{
 			green = false;
 			yellow = false;
 			purple = false;
+			brown = false;
+			orange = false;
 			red = true;
 		}
 		// if eraser (note: in top left corner)
@@ -176,6 +303,7 @@ public class Main extends PApplet{
 			yellow = false;
 			purple = false;
 			brown = false;
+			orange = false;
 			eraser = true;
 		}
 		// if blue
@@ -186,6 +314,7 @@ public class Main extends PApplet{
 			yellow = false;
 			purple = false;
 			brown = false;
+			orange = false;
 			blue = true;
 		}
 		// if green
@@ -195,6 +324,8 @@ public class Main extends PApplet{
 			blue = false;
 			yellow = false;
 			purple = false;
+			brown = false;
+			orange = false;
 			green = true;
 		}
 		// if yellow
@@ -205,6 +336,7 @@ public class Main extends PApplet{
 			green = false;
 			purple = false;
 			brown = false;
+			orange = false;
 			yellow = true;
 		}
 		// if purple
@@ -215,6 +347,7 @@ public class Main extends PApplet{
 			green = false;
 			yellow = false;
 			brown = false;
+			orange = false;
 			purple = true;
 		}
 		// if brown
@@ -225,8 +358,21 @@ public class Main extends PApplet{
 			green = false;
 			yellow = false;
 			purple = false;
+			orange = false;
 			brown = true;
 		}
+		// if orange
+		if(mouseX >= 0 && mouseX <= 50 && mouseY >= 350 && mouseY <= 400) {
+			eraser = false;
+			red = false;
+			blue = false;
+			green = false;
+			yellow = false;
+			purple = false;
+			brown = false;
+			orange = true;
+		}
+		
 	}
 	
 	// basically the same code for mousePressed
@@ -266,14 +412,19 @@ public class Main extends PApplet{
 			color2 = 42;
 			color3 = 42;
 		}
+		if(orange) {
+			color1 = 255;
+			color2 = 128;
+			color3 = 0;
+		}
 		else{
 			fill(0);
 		}
 		// check if mouse is in drawing area
-		if (mouseX >= 50 && mouseX <= 1280 && mouseY >= 0 && mouseY <= 680) {
+		if (mouseX >= 50 && mouseX <= 1280 && mouseY >= 50 && mouseY <= 680) {
 			// change the drawing color 
 			fill(color1, color2, color3);
-			rect(mouseX, mouseY, 50, 50);
+			rect(mouseX, mouseY, paintSize, paintSize);
 		}
 		// if red
 		if (mouseX >= 0 && mouseX <= 50 && mouseY >= 50 && mouseY <= 100) {
@@ -283,6 +434,8 @@ public class Main extends PApplet{
 			green = false;
 			yellow = false;
 			purple = false;
+			brown = false;
+			orange = false;
 			red = true;
 		}
 		// if eraser (note: in top left corner)
@@ -293,6 +446,7 @@ public class Main extends PApplet{
 			yellow = false;
 			purple = false;
 			brown = false;
+			orange = false;
 			eraser = true;
 		}
 		// if blue
@@ -303,6 +457,7 @@ public class Main extends PApplet{
 			yellow = false;
 			purple = false;
 			brown = false;
+			orange = false;
 			blue = true;
 		}
 		// if green
@@ -312,6 +467,8 @@ public class Main extends PApplet{
 			blue = false;
 			yellow = false;
 			purple = false;
+			brown = false;
+			orange = false;
 			green = true;
 		}
 		// if yellow
@@ -322,6 +479,7 @@ public class Main extends PApplet{
 			green = false;
 			purple = false;
 			brown = false;
+			orange = false;
 			yellow = true;
 		}
 		// if purple
@@ -332,6 +490,7 @@ public class Main extends PApplet{
 			green = false;
 			yellow = false;
 			brown = false;
+			orange = false;
 			purple = true;
 		}
 		// if brown
@@ -342,8 +501,20 @@ public class Main extends PApplet{
 			green = false;
 			yellow = false;
 			purple = false;
+			orange = false;
 			brown = true;
 		}
-	}
+		// if orange
+		if(mouseX >= 0 && mouseX <= 50 && mouseY >= 350 && mouseY <= 400) {
+			eraser = false;
+			red = false;
+			blue = false;
+			green = false;
+			yellow = false;
+			purple = false;
+			brown = false;
+			orange = true;
+		}
+	}	
 	
 }
